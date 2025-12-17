cb6cf98b2156fdce1e87be2655b813fcc3138112
Jeff Martin <jmartinpersonal@yahoo.com>
2025-12-17 09:44:02 -0500

    Update config

--- backend/dist/types/index.js
+++ backend/dist/types/index.js
@@ -1,6 +1,3 @@
 "use strict";
-// ============================================
-// src/types/index.ts - Type Definitions
-// ============================================
 Object.defineProperty(exports, "__esModule", { value: true });
 //# sourceMappingURL=index.js.map
